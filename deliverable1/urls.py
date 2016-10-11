b9cd36058c48ec0bd0b9d42327b81e42fbc80af9
Sean Floyd <seanlouisfloyd@gmail.com>
2016-10-11 12:37:47 -0400

    Resolve URL pattern warning

--- deliverable1/urls.py
+++ deliverable1/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^active_learning/', include('active_learning.urls')),
-    url(r'^$', include('active_learning.urls')),
+    url(r'^', include('active_learning.urls')),
     url(r'^admin/', admin.site.urls),
 ]
